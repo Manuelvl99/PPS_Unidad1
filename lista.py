1a7f98ebe21d550a24cb8025ca2ba567c4484561
Manuelvl99 <95217381+Manuelvl99@users.noreply.github.com>
2022-01-04 19:54:42 +0100

    Update lista.py

--- lista.py
+++ lista.py
@@ -3,17 +3,16 @@ dentro de la siguiente lista: [6,14,11,3,2,1,15,19]. Implementa una función que
 número introducido por el usuario está en el rango indicado y otra que compruebe si está dentro
 de la lista. Trata de crear las funciones de forma que puedan ser reutilizadas lo máximo posible en
 otros programas.'''
-#lista=(6,14,11,3,2,1,15,19)
-#valor=int(input("Escriba el número a buscar: "))
-#def estaEnLista(valor,lista):
-#    comprobacion=valor in lista
-#    return comprobacion  
-#print(estaEnLista(valor,lista))
-
-lista=[6,14,11,3,2,1,15,19]
+lista=(6,14,11,3,2,1,15,19)
 valor=int(input("Escriba el número a buscar: "))
 minimo=min(lista)
 maximo=max(lista)
+
+def estaEnLista(valor,lista):
+    comprobacion=valor in lista
+    return comprobacion  
+print(estaEnLista(valor,lista))
+
 def estaEnRango(valor,minimo,maximo):
     comprobacion=0
     if valor > minimo < maximo:
